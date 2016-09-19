b19e353dc3d1bdaf0bd6e5ab4ca0340e03c23ebf
UrbanSkurk <UrbanSkurk@HELL>
2016-09-19 09:47:49 +0200

    done

--- 06_Book_Titles/Book.cs
+++ 06_Book_Titles/Book.cs
@@ -1,4 +1,6 @@
-﻿namespace _06_Book_Titles
+﻿using System.Linq;
+
+namespace _06_Book_Titles
 {
     internal class Book
     {
@@ -8,6 +10,33 @@
         public Book()
         {
         }
+
+        //Properties
+
+        private string Capitalize(string s)
+        {
+            string[] blacklist = {"the", "a", "an", "and", "in", "of" };
+            string[] words = s.Split();
+
+            string cappedwords = words[0].Substring(0,1).ToUpperInvariant()
+                + words[0].Substring(1) + " ";
+
+            for (int i = 1; i < words.Length; i++)
+            {
+                if (!blacklist.Contains(words[i]))
+                {
+                    cappedwords += words[i].Substring(0, 1).ToUpperInvariant()
+                                + words[i].Substring(1) + " ";
+                }
+                else
+                {
+                    cappedwords += words[i] + " ";
+                }
+            }
+
+           
+            return cappedwords.Trim(); 
+        }
         
         //Properties
         public string Title
@@ -15,8 +44,8 @@
             get { return title; }
             set
             {
-                title = value.Substring(0, 1).ToUpperInvariant() 
-                    + value.Substring(1);
+                title = Capitalize(value);
+
             }
         }
 
