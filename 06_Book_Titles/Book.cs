e0c6e010e3eaf9efb7fdab547460a726808f6288
UrbanSkurk <UrbanSkurk@HELL>
2016-09-19 09:04:05 +0200

    first test done.

---
+++ 06_Book_Titles/Book.cs
@@ -0,0 +1,24 @@
+﻿namespace _06_Book_Titles
+{
+    internal class Book
+    {
+        //Fields
+        private string title;
+        //Constructor
+        public Book()
+        {
+        }
+        
+        //Properties
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value.Substring(0, 1).ToUpperInvariant() 
+                    + value.Substring(1);
+            }
+        }
+
+    }
+}
